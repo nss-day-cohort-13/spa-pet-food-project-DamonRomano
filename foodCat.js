6920a4433c8d973ee363ef5a659fdc0b8e09a1a8
Damon Romano <rdromano@mac.com>
2016-05-10 15:36:55 -0500

    Got dog food working; starting on cat food

--- foodCat.js
+++ foodCat.js
@@ -1,34 +1,41 @@
-// CAT FOOD
 
-var catFood = document.getElementById("cat_brands");
+'use-strict';
+// CAT FOOD
+var catListBuilder = "";
+
 function displayCatFood () {
+  var catFood = document.getElementById("cat_brands");
+
   var catData = JSON.parse(this.responseText);
   // console.log(catData);
   for (var a = 0; a < catData.cat_brands.length; a++) {
     // console.log("catData.cat_brands",catData.cat_brands[a]);
+
     var name = catData.cat_brands[a].name;
-    console.log("Catdata name", name );
+    console.log("catData name", name );
+    catListBuilder += `<div class="row-fluid typeCenter brandBox"><h1>${name}</h1>`;
 
     var typeArr = catData.cat_brands[a].types;
     for (var e = 0; e < typeArr.length; e++) {
       console.log("type",typeArr[e].type);
+      catListBuilder += `<div class="row-fluid col-md-6 typeCenter"><h3>${typeArr[e].type}</h3>`;
 
       var volumes = typeArr[e].volumes;
       for (var i = 0; i < volumes.length; i++) {
         console.log("volumes name",volumes[i].name);
         console.log("volumes price",volumes[i].price);
-      }
-    }
-  }
+        catListBuilder += `<div class="row-fluid volume col-sm-6"><p>${volumes[i].name}</p><p>${volumes[i].price}</p>`;
 
+        catListBuilder += `</div>`;
+      }
+      catListBuilder += `</div>`;
+    }
+    catListBuilder += `</div>`;
+  }
+  catFood.innerHTML = catListBuilder;
 }
 
 var cat = new XMLHttpRequest();
-  cat.addEventListener("load", displayCatFood);
-  cat.open("GET", "catFood.json");
-  cat.send();
-
-
-
-
-
+cat.addEventListener("load", displayCatFood);
+cat.open("GET", "catFood.json");
+cat.send();
--- foodDog.js
+++ foodDog.js
@@ -1,32 +1,45 @@
-// DOG FOOD
 
-var dogFood = document.getElementById("dog_brands");
+'use-strict';
+// DOG FOOD
+var dogListBuilder = "";
+
 function displayDogFood () {
+  var dogFood = document.getElementById("dog_brands");
+
   var dogData = JSON.parse(this.responseText);
   // console.log(dogData);
   for (var a = 0; a < dogData.dog_brands.length; a++) {
     // console.log("dogData.dog_brands",dogData.dog_brands[a]);
+
     var name = dogData.dog_brands[a].name;
     console.log("Dogdata name", name );
+    dogListBuilder += `<div class="row-fluid typeCenter brandBox"><h1>${name}</h1>`;
 
     var typeArr = dogData.dog_brands[a].types;
     for (var e = 0; e < typeArr.length; e++) {
       console.log("type",typeArr[e].type);
+      dogListBuilder += `<div class="row-fluid col-md-6 typeCenter"><h3>${typeArr[e].type}</h3>`;
 
       var volumes = typeArr[e].volumes;
       for (var i = 0; i < volumes.length; i++) {
         console.log("volumes name",volumes[i].name);
         console.log("volumes price",volumes[i].price);
-      }
-    }
-  }
+        dogListBuilder += `<div class="row-fluid volume col-sm-6"><p>${volumes[i].name}</p><p>${volumes[i].price}</p>`;
 
+        dogListBuilder += `</div>`;
+      }
+      dogListBuilder += `</div>`;
+    }
+    dogListBuilder += `</div>`;
+  }
+  dogFood.innerHTML = dogListBuilder;
 }
 
 var dog = new XMLHttpRequest();
-  dog.addEventListener("load", displayDogFood);
-  dog.open("GET", "dogFood.json");
-  dog.send();
+dog.addEventListener("load", displayDogFood);
+dog.open("GET", "dogFood.json");
+dog.send();
+
 
 
 
